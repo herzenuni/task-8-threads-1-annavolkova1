6ead228a86c8e25992d32846a24b7ae67e147c4a
annavolkova1 <anna__banana@mail.ru>
2017-06-26 22:22:18 +0300

    matrix

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-def matrix_multiply(X, Y):
-    """ здесь необходимо написать доктест """
-    # Z = X * Y
-    pass
+import threading
 
+X = [[9, 8], [7, 6]]
+Y = [[5, 4], [3, 2]]
+
+def calculate_element(row, col):
+    Z = 0
+
+    for i, item in enumerate(row):
+        Z += item * col[i]
     return Z
 
+def calculate_row(rowA, Y):
+    cols = [[row[i] for row in Y] for i in range(len(Y[0]))]
+    Z = list(map(lambda x: calculate_element(rowA, x), cols))
+    print(Z)
+    return Z
 
-if __name__ == '__main__':
-    # здесь необходимо написать код
-    # для тестирования
+for row in X:
+    threading.Thread(target=calculate_row, args=(row, Y)).start()
